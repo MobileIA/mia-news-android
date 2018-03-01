68d2ef43b4a2ca103240a88750b1647cd1977244
Matias Camiletti <matias.camiletti@gmail.com>
2018-03-01 13:19:16 -0300

    Fix para poder obtener las noticias del servidor.

--- news/src/main/java/com/mobileia/news/rest/service/NewsService.java
+++ news/src/main/java/com/mobileia/news/rest/service/NewsService.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 /**
@@ -15,8 +16,7 @@ import retrofit2.http.POST;
 
 public interface NewsService {
 
-    @FormUrlEncoded
-    @POST("api/news/list")
+    @GET("api/news/list")
     RestBodyCall<ArrayList<News>> fetchAllCurrent();
 
 }
